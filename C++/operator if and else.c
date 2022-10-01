4df7ce3b5ce3aef72d270e2e7c53b2349151781b
asghar-khan-coder <114768857+asghar-khan-coder@users.noreply.github.com>
2022-10-01 12:09:43 +0530

    Add files via upload

---
+++ C++/operator if and else.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+int main()
+{
+    int n=12;
+   int result=n%2; //od operator which returns reminder
+    if(result==0)
+    {
+    printf("even num");
+    }
+    else{
+ printf("odd num");
+    }
+
+    //printf("%d\n",n%2==0); //this statement will returns either 0 or 1 (0 is for false and 1 is for true)
+
+
+   // if(n%2==0)
+    //{
+       // printf("even no");
+
+  // }
+   //else
+    //{
+
+           // printf("odd no");
+//}
+
+//printf("%d\n",1==0);
+
+}
+
+
+
+
+
+
+
+
+
+
