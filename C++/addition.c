a899371b01f0ad8dce12fc40d72a82fadc92682e
asghar-khan-coder <114768857+asghar-khan-coder@users.noreply.github.com>
2022-10-01 12:03:03 +0530

    Add files via upload

---
+++ C++/addition.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+ int main()
+{
+    int a=20;
+    int b=10;
+    int resultsum=a+b;
+
+    printf("queient %d\n",a/b);
+    printf("reminder %d\n",a,b);
+
+    printf("%d\n",resultsum);
+    printf("a %d + b %d= %d\n",a,b,resultsum);
+
+
+}
