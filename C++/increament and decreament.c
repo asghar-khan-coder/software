e61055d0fd59b4789a253784e3133024382b78f0
asghar-khan-coder <114768857+asghar-khan-coder@users.noreply.github.com>
2022-10-01 12:04:42 +0530

    Add files via upload

---
+++ C++/increament and decreament.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+    int n1=10;
+    int n2=2;
+    int resultsum=n1+n2;
+    printf("%d\n",resultsum);
+    printf("n1 %d + n2 %d = %d\n",n1,n2,resultsum);
+
+
+    printf(" quetient- %d\n",n1/n2);
+    printf("reminder- %d\n",n1%n2);
+
+
+    printf("%d\n",n1++);
+    printf("%d\n",n1);
+
+    printf("%d\n",++n1);
+    printf("%d\n",n1);
+
+    printf("%d\n",n2--);
+    printf("%d\n",n2);
+
+    printf("%d\n",--n2);
+    printf("%d\n",n2);
+}
