29db2b0c996970ddbe12a4e5fd227d07f9190835
asghar-khan-coder <114768857+asghar-khan-coder@users.noreply.github.com>
2022-10-01 12:13:01 +0530

    Add files via upload

---
+++ C++/while loop.c
@@ -0,0 +1,70 @@
+#include<stdio.h>
+int main()
+{
+
+ //int n=1;
+    //while(1)
+        //{
+        //printf("loop runs.........");
+
+    //}
+
+    //{
+       //printf("*");
+   //}
+
+
+
+
+
+
+   //int n=1;
+   //while(n<10){
+    //int i=0;   // this is called floid triangle
+    //while(i<n){
+        //printf("*\t");
+        //i++;
+    //}
+
+
+
+    //printf("\n");
+    //n++;
+//}
+
+
+//int n=10;
+//while(n>0){
+       // printf("%d\t",n); // this will return decreaminent eg:10,9,8....
+         //n--;
+//}
+
+
+
+
+//int n=10;
+//while(n>0){
+  //      int i=0;
+//while(i<n){
+  //      i++;
+//printf("%d\t",n);
+//}
+//printf("\n");
+//n--;
+//}         //this is called outer loop
+
+
+
+
+int x=3;
+int y=3;
+while(x>0){
+    x--;
+    printf("outer loop....");
+
+   while(y>0){
+    y--;
+    printf("\inner loop....\n");
+   }
+}
+}
