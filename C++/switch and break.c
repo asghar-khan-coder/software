6ba1107eb2167560605702ae666f9d2f547b1c32
asghar-khan-coder <114768857+asghar-khan-coder@users.noreply.github.com>
2022-10-01 12:11:36 +0530

    Add files via upload

---
+++ C++/switch and break.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+int main()
+{
+    int day=4;
+    switch(day){
+        case 0:{
+        printf("sunday\n...");
+        break;
+}
+case 1:{
+    printf("monday...");
+    break;
+}
+
+case 2:{
+    printf("tuesday...");
+    break;
+}
+
+case 3:{
+printf("wednesday...");
+break;
+}
+
+case 4:{
+printf("thusday...");
+break;
+}
+
+default:{
+printf("this is default...");
+}
+    }
+}
+
+
+
