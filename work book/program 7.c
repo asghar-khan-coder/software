47115a357a4294781dec0755bc60cf3e90cbbb18
asghar-khan-coder <114768857+asghar-khan-coder@users.noreply.github.com>
2022-10-03 23:57:21 +0530

    Add files via upload

---
+++ work book/program 7.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    char chr;
+    printf("Enter a character: ");
+    scanf("%c",&chr);
+
+    printf("you entered %c",chr);
+    printf("ACSII value is %d",chr);
+    return 0;
+
+}
