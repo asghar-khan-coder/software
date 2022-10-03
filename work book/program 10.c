707abf69a7ca629b681011fedbfaf58c77290cda
asghar-khan-coder <114768857+asghar-khan-coder@users.noreply.github.com>
2022-10-03 23:58:31 +0530

    Add files via upload

---
+++ work book/program 10.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+    int a;
+    printf("Enter an integer\n");
+    scanf("%d", &a);
+    printf("Integer that you have entered is %d\n", a);
+}
