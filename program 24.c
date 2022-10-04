3f8a4990fd3d25252451ba7a7f354d631e7b490a
asghar-khan-coder <114768857+asghar-khan-coder@users.noreply.github.com>
2022-10-04 17:09:42 +0530

    Add files via upload

---
+++ program 24.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int number;
+    printf("Enter an integer: ");
+    scanf("%d",&number);
+
+    if(number%2 == 0){
+        printf("%d is an even number.",number);
+     }else{
+         printf("%d is an odd integer.",number);
+     }
+     return 0;
+}
