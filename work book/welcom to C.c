5b41ebdd4541aae09382fcc0caafcf533413a29d
asghar-khan-coder <114768857+asghar-khan-coder@users.noreply.github.com>
2022-10-04 17:40:33 +0530

    Add files via upload

---
+++ work book/welcom to C.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    printf("Welcome to C programming");
+    int age=26;
+    char name[10]="Asgharkhan";
+    printf("\nMy Name is :%s",name);
+    printf("\nMy Age is :%d",age);
+    char c='a';
+    char cc=100;
+    const double pi=3.14;
+    printf("\nMy PI value is :%lf",pi);
+    printf("\nThe size of pi value is :%d",sizeof(pi));
+}
