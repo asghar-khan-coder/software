fa2f3c0ecae9f3157abcffb2b055e6271704bd5a
asghar-khan-coder <114768857+asghar-khan-coder@users.noreply.github.com>
2022-10-04 17:09:17 +0530

    Add files via upload

---
+++ work book/program 23.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int number;
+    printf("Enter an integer: ");
+    scanf("%d", &number);
+
+    if(number < 0){
+        printf("You entered %d.\n", number);
+    }
+    printf("The if statement is easy.");
+
+    return 0;
+}
